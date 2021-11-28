e0bb57c5ed856b2962586116abf548d2c21f6a68
rob49157 <robertopaipo@hotmail.com>
2021-11-28 13:07:53 -0500

    server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const Sequalize = require('sequelize')
+const sequelize = require('./config/connection')
 const bcrypt = require('bcrypt')
 const cookieparser= require('express-session')
 const morgan = require("morgan")
-const User = require ('./models/user')
-const handlebars= require('express-handlebars')
+const exphbs = require('express-handlebars');
 const bodyparser = require('body-parser')
 const express= require('express')
 const path= require('path')
 const routes = require('./controllers');
-const { Sequelize } = require('sequelize');
+//const { Sequelize } = require('sequelize');
 const User = require('./models/user')
-
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 // import sequelize connection
 
@@ -20,18 +20,35 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser())
-
-//track user across session
-app.use (session({
-  key: 'user_sid',
-  secret: 'somerandonstuffs',
+// app.use (session({
+//   //key: 'user_sid',
+//   secret: 'somerandonstuffs',
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//       expires: 600000}
+// }))
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
   resave: false,
-  saveUninitialized: false,
-  cookie: {
-      expires: 600000}
-}))
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
+app.use(session(sess));
 
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieparser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 const sequelize = new Sequelize('crypto_table', 'root', 'dal123456789+', {
   host: 'localhost',
