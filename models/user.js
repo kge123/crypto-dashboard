1fa6d4a3a90c6142eb3f1b0b28ce3f06188c83b5
kge123 <88997999+kge123@users.noreply.github.com>
2021-11-30 21:28:43 -0500

    trying to correct login functionality

--- models/user.js
+++ models/user.js
@@ -1,20 +1,24 @@
-var Sequalize = require('sequelize')
-var bcrypt = require('bcrypt')
-var cookieparser= require('express-session')
-var morgan = require("morgan")
-var User = require ('./models/user')
-var handlebars= require('express-handlebars')
-var bodyparser = require('body-parser')
-var express= require('express')
-var path= require('path')
+// var Sequalize = require('sequelize')
+// var bcrypt = require('bcrypt')
+// var cookieparser= require('express-session')
+// var morgan = require("morgan")
+// var User = require ('./models/user')
+// var handlebars= require('express-handlebars')
+// var bodyparser = require('body-parser')
+// var express= require('express')
+// var path= require('path')
 
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
-
-// table 
+// table
 User.init(
   {
     id: {
@@ -45,30 +49,43 @@ User.init(
     },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: "user",
   }
 );
 
 //hashing password
 
-User.beforeCreate((user,option)=>{
-    const salt= bcrypt.genSaltSync();
-    user.password = bcrypt.hashSync(user.password,salt)
-})
+// User.beforeCreate((user,option)=>{
+//     const salt= bcrypt.genSaltSync();
+//     user.password = bcrypt.hashSync(user.password,salt)
+// })
 
-// create all defined tables in database
-User.prototype.validatepassword=function(password){
-    return bcrypt.compareSync(password, this.password)
-}
+// // create all defined tables in database
+// User.prototype.validatepassword=function(password){
+//     return bcrypt.compareSync(password, this.password)
+// }
 
-sequelize.sync()
-.then(()=> console.log("account added to table"))
-.catch(console.error())
-
+// sequelize
+//   .sync()
+//   .then(() => console.log("account added to table"))
+//   .catch(console.error());
 
 // export module
 module.exports = User;
