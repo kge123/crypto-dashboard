5a1e3d37188c3fd952a58b0111851df4959b541e
Jrtaylor17 <Jrtaylor17@gmail.com>
2021-12-08 21:16:49 -0500

    watchlist

--- public/js/search.js
+++ public/js/search.js
@@ -8,16 +8,28 @@ async function searchFormHandler(e) {
        return response.json()
    })
     //console.log of input
-    let x = document.getElementsByClassName('coin');
-     console.log({prices}) 
-    for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="list-item";                 
-        }
-    }
+    //let x = document.getElementsByClassName('coin');
+    console.log({prices}) 
+//filter list based of match criteria
+
+
+//DISPLAY search results with check box or ???
+
+
+//allow user to save favorites
+
+
+//make service call to update favorites
+
+
+///for (i = 0; i < x.length; i++) { 
+    //     if (!x[i].innerHTML.toLowerCase().includes(input)) {
+    //         x[i].style.display="none";
+    //     }
+    //     else {
+    //         x[i].style.display="list-item";                 
+    //     }
+    // }
 
 
 }
