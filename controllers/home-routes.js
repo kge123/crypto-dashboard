e9b5eb0c3b9bd2686c0cea8c50a9591ac2acb7d7
kge123 <88997999+kge123@users.noreply.github.com>
2021-12-10 22:13:56 -0500

    format/readme placeholders

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,14 @@
 const router = require("express").Router();
 const { Model } = require("sequelize");
-const {Coin, User }= require('../models')
+const { Coin, User } = require("../models");
 const withAuth = require("../utils/auth");
-
 
 router.get("/", async (req, res) => {
   res.render("homepage", { loggedIn: req.session.loggedIn });
-
 });
 
 router.get("/search", async (req, res) => {
   res.render("searchcrypto", { loggedIn: req.session.loggedIn });
-
 });
 // rendering sql data
 
@@ -25,7 +22,6 @@ router.get("/search", async (req, res) => {
 //       }
 //     ],
 //   })
-   
 
 // const coins= coindata.map((coin)=>coin.get({plain: true}))
 // res.render('APIDATA'),{
@@ -38,37 +34,33 @@ router.get("/search", async (req, res) => {
 
 router.get("/dashboard", withAuth, async (req, res) => {
   res.render("dashboard", { loggedIn: req.session.loggedIn });
-  
-  
 });
 
 // display from database
 router.get("/definitions", withAuth, async (req, res) => {
   res.render("definitions", { loggedIn: req.session.loggedIn });
-  
-  const coindata =await Coin.findAll({
-    
-    include:[
-      {Model:User,
-      attributes:['name']}
-    ]
-    
-    })
-    
-    console.log('dadasdasd')
-    const coins= coindata.map((coinDefinitions)=>coinDefinitions.get({plain: true}))
-      res.render('APIDATA'),{
+
+  const coindata = await Coin.findAll({
+    include: [{ Model: User, attributes: ["name"] }],
+  });
+
+  console.log("dadasdasd");
+  const coins = coindata.map((coinDefinitions) =>
+    coinDefinitions.get({ plain: true })
+  );
+  res.render("APIDATA"),
+    {
       coinDefinitions,
-      loggedIn:req.session.loggedIn
-    }
+      loggedIn: req.session.loggedIn,
+    };
 });
 
 router.get("/login", (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect("/");
-        return;
-    }
-    res.render("login");
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
 });
 
 module.exports = router;
